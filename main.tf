1f97b9a87f06c47cf2ec9dd6c34024c536c8be83
sergiimatusEPAM <sergii_matus@epam.com>
2021-03-09 21:29:51 +0200

    changed approach of deploying to Azure

--- main.tf
+++ main.tf
@@ -1,95 +1,80 @@
+
+terraform {
+  required_version = ">=0.12.29"
+  backend "azurerm" {}
+}
+
 provider "azurerm" {
-  version                    = ">= 2.0"
-  skip_provider_registration = true
+  version = "2.26.0"
   features {}
-
 }
 
-data "azurerm_resource_group" "demo-rg" {
-  name = "${var.region}-${var.id}-application-resources"
+provider "random" {
+  version = "~> 2.3"
 }
 
-output "resource_group_id" {
-  value = data.azurerm_resource_group.demo-rg.id
+locals {
+  global_tags = merge({ environment = var.vm_name }, var.environment_tags)
 }
 
-resource "azurerm_virtual_network" "test" {
-  name                = "acctvn"
-  address_space       = ["10.0.0.0/16"]
-  resource_group_name = data.azurerm_resource_group.demo-rg.name
-  location            = data.azurerm_resource_group.demo-rg.location
+data "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = var.vnet_resource_group_name
 }
 
-resource "azurerm_subnet" "test-subnet" {
-  name                 = "acctsub"
-  resource_group_name  = data.azurerm_resource_group.demo-rg.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.2.0/24"]
+resource "azurerm_network_interface" "nic" {
+
+    name                         = "${var.vm_name}-nic-${var.location}"
+    location                     = var.location
+    resource_group_name          = var.resource_group_name
+    tags                         = local.global_tags
+
+    ip_configuration {
+        name                          = "${var.vm_name}-nic-config-${var.location}"
+        subnet_id                     = data.azurerm_subnet.azurerm_subnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.publicip.id
+    }
 }
 
-resource "azurerm_network_interface" "test-network" {
-  name                = "acctni"
-  resource_group_name = data.azurerm_resource_group.demo-rg.name
-  location            = data.azurerm_resource_group.demo-rg.location
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.test-subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
+data "azurerm_subnet" "azurerm_subnet" {
+  name                 = var.azurerm_subnet_name
+  virtual_network_name = var.vnet_name
+  resource_group_name  = var.vnet_resource_group_name
 }
 
-resource "azurerm_storage_account" "test-storage-account" {
-  name                     = "storageaccount"
-  resource_group_name      = data.azurerm_resource_group.demo-rg.name
-  location                 = data.azurerm_resource_group.demo-rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "staging"
-  }
+resource "azurerm_public_ip" "publicip" {
+    name                         = "${var.vm_name}-ip-${var.location}"
+    location                     = var.location
+    resource_group_name          = var.resource_group_name
+    allocation_method            = "Dynamic"
+    tags                         = local.global_tags
 }
 
-resource "azurerm_storage_container" "test-container" {
-  name                  = "vhds"
-  storage_account_name  = azurerm_storage_account.test-storage-account.name
-  container_access_type = "private"
-}
+resource "azurerm_windows_virtual_machine" "vm" {
 
+    name                         = "${var.vm_name}-vm-${var.location}"
+    location                     = var.location
+    resource_group_name          = var.resource_group_name
+    tags                         = local.global_tags
 
-resource "azurerm_virtual_machine" "test-vm" {
-  name                  = "testvm"
-  resource_group_name   = data.azurerm_resource_group.demo-rg.name
-  location              = data.azurerm_resource_group.demo-rg.location
-  network_interface_ids = [azurerm_network_interface.test-network.id]
-  vm_size               = "Standard_F2"
+    network_interface_ids 		 = [azurerm_network_interface.nic.id]
+    size                         = var.vm_size
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
+    computer_name                = var.vm_name
+    admin_username               = var.admin_username
+    admin_password               = var.admin_password
 
-  storage_os_disk {
-    name          = "myosdisk1"
-    vhd_uri       = "${azurerm_storage_account.test-storage-account.primary_blob_endpoint}${azurerm_storage_container.test-container.name}/myosdisk1.vhd"
-    caching       = "ReadWrite"
-    create_option = "FromImage"
-  }
+    os_disk {
+        name                     = "${var.vm_name}-os-disk-${var.location}"
+        caching                  = "ReadWrite"
+        storage_account_type     = "Standard_LRS"
+    }
 
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "N0M@tt3rPassword54321"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags = {
-    environment = "staging"
+    source_image_reference {
+       publisher = "MicrosoftWindowsServer"
+       offer     = "WindowsServer"
+       sku       = "2019-Datacenter"
+       version   = "latest"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,49 @@
 variable "region" { type = string }
 variable "id" { type = string }
+
+variable "subscription_id" {
+  type        = string
+  description = "azure subscription to deploy all infra to"
+}
+variable "location" {
+  type        = string
+  description = "azure region to deploy all infra to"
+}
+variable "resource_group_name"{
+  type        = string
+  description = "VM resource group name"
+}
+variable "vm_name" {
+  type        = string
+  description = "the base name of VM to deploy"
+}
+variable "vm_size" {
+  type        = string
+  default     = "Standard_D2_v4"
+  description = "the size of VM to deploy"
+}
+variable "admin_username" {
+  type        = string
+  description = "the administrator's user name"
+}
+variable "admin_password" {
+  type        = string
+  description = "the administrator's password"
+}
+variable "vnet_name"{
+  type        = string
+  description = "vnet name"
+}
+variable "vnet_resource_group_name"{
+  type        = string
+  description = "vnet resource group name"
+}
+variable "azurerm_subnet_name"{
+  type        = string
+  description = "subnet name"
+}
+variable "environment_tags" {
+  type        = map(string)
+  description = "tags to be added to all resources created by this environment"
+  default     = {}
+}
